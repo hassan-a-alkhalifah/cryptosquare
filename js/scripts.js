ac14e36a34c1a0bffa103af2000990560f4680ee
Hassan Al-khalifah, Kate Goss and Luke Vandekieft <luke@>
2018-08-16 13:44:42 -0700

    write JS to convert numbers 1-9 into roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,23 @@ var numberPlace1 = ["I", "V", "X"]
 var numberPlace10 = ["X", "L", "C"]
 var numberPlace100 = ["C", "D", "M"]
 var numberPlace1000 = ["M"]
-
-
 var translateDigit = function(digit, place) {
-  console.log(digit + " " + typeof place);
+  var counter = "";
+
   if (digit === "9") {
     return digit = place[0] + place[2];
+  } else if (digit === "4") {
+    return digit = place[0] + place[1];
+  } else if (parseInt(digit) < 4) {
+    for (var i = 0; i < parseInt(digit); i++) {
+      counter += place[0];
+    }
+    return counter;
+  } else if (5 < parseInt(digit) || parseInt(digit) < 9) {
+    for (var i = 5; i < parseInt(digit); i++) {
+      counter += place [0];
+    }
+    return place[1] + counter;
   } else {
     alert("Error");
   }
@@ -18,8 +29,6 @@ var translateDigit = function(digit, place) {
 
 //   if digit between 5-8
 //     digit place3 [1] + place3 [0]x(digit-5)
-//   if digit === 4
-//     show place 3 [0] + place3 [1]
 //   if digit 0-3 place3 [0]x(digit)
 // }
 
